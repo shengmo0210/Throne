135633301826bbd2403d5309db0b8b0d559ad0f6
Nova <mahdi.zrei@gmail.com>
2025-02-28 17:03:12 +0330

    fix dns monitor stub

--- core/server/internal/boxdns/monitor_stub.go
+++ core/server/internal/boxdns/monitor_stub.go
@@ -4,10 +4,11 @@ package boxdns
 
 import (
 	tun "github.com/sagernet/sing-tun"
+	"github.com/sagernet/sing/common/control"
 )
 
 var DefaultIfcMonitor tun.DefaultInterfaceMonitor
 
-func monitorForUnderlyingDNS() {
+func HandleInterfaceChange(_ *control.Interface, _ int) {
 	return
 }
