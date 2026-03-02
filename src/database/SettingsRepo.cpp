54c4e66ada71020164bca85e975201bc268541ba
parhelia512 <0011d3@gmail.com>
2026-03-02 20:39:23 +0800

    Update user agent string
    Removed '(Prefer ClashMeta Format)' from user agent string.

--- src/database/SettingsRepo.cpp
+++ src/database/SettingsRepo.cpp
@@ -501,7 +501,7 @@ namespace Configs {
         if (isDefault) {
             QString version = SubStrBefore(NKR_VERSION, "-");
             if (!version.contains(".")) version = "1.0.0";
-            return "Throne/" + version + " (Prefer ClashMeta Format)";
+            return "Throne/" + version;
         }
         return user_agent;
     }
